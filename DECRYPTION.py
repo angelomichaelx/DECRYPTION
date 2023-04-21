000d760525c6290d0f7d70aec92e7000d71c62aa
Michael Angelo P. Biag <michaelangelobiag23@GMAIL.COM>
2023-04-21 17:28:30 +0800

    user asked to input the string

---
+++ DECRYPTION.py
@@ -0,0 +1,18 @@
+#Michael Angelo P. Biag
+#BSCOE 1-4
+
+#Problem number 2 :DECRYPTION (25 points)
+
+#this is where the user ask to input the string
+input_string = input ("INPUT YOUR STRING? ")
+output_string = ""
+
+
+#to check every character
+#if the input string is  *, then change it to letter a
+#if the input string is  &, then change it to letter e
+#if the input string is  #, then change it to letter i
+#if the input string is  +, then change it to letter o
+#if the input string is  !, then change it to letter u 
+#print output with decoration
+#program if you want to repeat it again
